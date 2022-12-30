fdd0d8f2705bffdf1e13cd640166bc5dce1a1dba
FearFactor1 <zelezodoroznik@yandex.ru>
2022-12-30 15:52:51 +0300

    Задание 3

--- src/main/java/practice/Main.java
+++ src/main/java/practice/Main.java
@@ -1,9 +1,46 @@
 package practice;
 
+import java.util.Scanner;
+
 public class Main {
     private static TodoList todoList = new TodoList();
 
     public static void main(String[] args) {
         // TODO: написать консольное приложение для работы со списком дел todoList
+        boolean isTrue = true;
+        System.out.print("Введите команду: ");
+        Scanner scan = new Scanner(System.in);
+        while (isTrue) {
+            String typeCommand = scan.nextLine();
+            if (typeCommand.equals("exit")) {
+                isTrue = false;
+            }
+            if (typeCommand.contains("ADD")) {
+                String tpCom = typeCommand.replace("ADD ", "");
+                todoList.add(tpCom);
+                System.out.println("Добавлено дело " + '"' + tpCom + '"');
+            }
+            if (typeCommand.contains("LIST")) {
+                for (int i = 0; i < todoList.getTodos().size(); i++) {
+                    System.out.println(i + " - " + todoList.getTodos().get(i));
+                }
+            }
+            if (typeCommand.contains("EDIT")) {
+                String tpCom = typeCommand.replaceAll( "[0-9,EDIT]", "").strip();
+                String[] tpInd = typeCommand.split(" ");
+                System.out.println("Дело " + '"' + todoList.getTodos().get(Integer.parseInt(tpInd[1])) +
+                        '"' + " заменено на " + '"' + tpCom + '"');
+                todoList.edit(Integer.parseInt(tpInd[1]), tpCom);
+            }
+            if (typeCommand.contains("DELETE")) {
+                String[] tpInd = typeCommand.split(" ");
+                if (Integer.parseInt(tpInd[1]) > todoList.getTodos().size()) {
+                    System.out.println("Дело с таким номером не существует");
+                } else {
+                    System.out.println("Дело " + '"' + todoList.getTodos().get(Integer.parseInt(tpInd[1])) + '"' + " удалено");
+                    todoList.delete(Integer.parseInt(tpInd[1]));
+                }
+            }
+        }
     }
 }
--- src/main/java/practice/TodoList.java
+++ src/main/java/practice/TodoList.java
@@ -4,28 +4,48 @@ import java.util.ArrayList;
 
 public class TodoList {
 
+    ArrayList<String> todoList = new ArrayList<>();
+
     public void add(String todo) {
         // TODO: добавьте переданное дело в конец списка
+        todoList.add(todo);
     }
 
     public void add(int index, String todo) {
         // TODO: добавьте дело на указаный индекс,
         //  проверьте возможность добавления
+        if (index > todoList.size()) {
+            todoList.add(todo);
+        } else {
+            todoList.add(index, todo);
+        }
     }
 
     public void edit(int index, String todo) {
         // TODO: заменить дело на index переданным todo индекс,
         //  проверьте возможность изменения
+        if (index >= todoList.size()) {
+            return;
+        } else {
+            todoList.set(index, todo);
+        }
     }
 
     public void delete(int index) {
         // TODO: удалить дело находящееся по переданному индексу,
         //  проверьте возможность удаления дела
+        if (index >= todoList.size()) {
+            return;
+        } else {
+            todoList.remove(index);
+        }
     }
 
     public ArrayList<String> getTodos() {
         // TODO: вернуть список дел
+        for (int i = 0; i < todoList.size();) {
+            return todoList;
+        }
         return new ArrayList<>();
     }
-
 }
